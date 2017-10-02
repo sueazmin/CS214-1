75d84b1f8c7c8981cdf7dd3f8a03902acee232cf
sueazmin <suhailazmin96@gmail.com>
2017-10-01 20:45:04 -0400

    commented

--- Sorter.c
+++ Sorter.c
@@ -1,55 +1,83 @@
+/**
+* Group members: Suhail Azmin Bin Saiful adli & Maryam Farid Iskandar
+* Class: CS 214
+* File: Sorter.c
+* Description: Sorter.c is the main file that calls the all the functions from 
+*              Mergesort.c with the help of Sorter.h to record the data from 
+*              movie_metadata.csv and sort them using merge sort algorithm.
+*
+**/
+
+
+
+
+
+
 #include "sorter.h"
 
  
 int main(int argc, char *argv[]) {
+    
     FILE *stream = stdin;
+
+    //Hold each line from movie_metadata.csv file
     char line[200000];
-    int size = 0, len = 0, count = 0, i, c;
-    struct item_t *A = NULL;
+
+    int size = 0, len = 0, count = 0;
+
+    //struct pointer to hold every data from movie_metadata.csv file
+    struct item_t *Record = NULL;
+
+    //hold first line of movie_metadata.csv file which is the coloumn types.
     char token[200000];
-    int aha = 1;
-    int olololo = 0;
+    int first_row = 1;
+    int true_size = 0;
 
 
     if (stream) {
         while (fgets(line, sizeof(line), stream)) {
-            if (aha ==1)
+            if (first_row ==1)
             {
-                aha++;
+                first_row++;
                 strcpy(token,line);
                 continue;
             }
-            olololo++;
+            true_size++;
 
+
+            //allocate memory for struct item_t pointer
             if (len == size) {
                 size = size ? size * 2 : 1000;
-                A = realloc(A, sizeof(*A) * size);
-                if (A == NULL) {
+                Record = realloc(Record, sizeof(*Record) * size);
+                if (Record == NULL) {
                     fprintf(stderr, "out of memory for %d items\n", size);
                     return 1;
                 }
             }
+
+            //scan each line until a comma that seperates between two data and 
+            //seperate them into designated struct member variables.
             if (sscanf(line, "%190[^,\n],%190[^,\n],%190[^,\n],%190[^,\n],%230[^,\n],%400[^,\n],%490[^,\n],%490[^,\n],%490[^,\n],%490[^,\n],%490[^,\n],%490[^,],%490[^,\n],%490[^,\n],%490[^,\n],%490[^,\n],%490[^,\n],%499[^,\n],%490[^,\n],%490[^,\n],%490[^,\n],%490[^,\n],%490[^,\n],%490[^,\n],%490[^,\n],%490[^,\n],%490[^,\n],%490[^,\n]",
-                       A[len].color, A[len].director_name, &(A[len].num_critic_for_reviews),A[len].duration,
-                       A[len].director_facebook_likes,A[len].actor_3_facebook_likes,A[len].actor_2_name,
-                       A[len].actor_1_facebook_likes,A[len].gross,A[len].genres,A[len].actor_1_name,
-                       A[len].movie_title,A[len].num_voted_users,A[len].cast_total_facebook_likes,A[len].actor_3_name,
-                       A[len].facenumber_in_poster,A[len].plot_keywords,A[len].movie_imdb_link,A[len].num_user_for_reviews,
-                       A[len].language,A[len].country,A[len].content_rating,A[len].budget,A[len].title_year,
-                       A[len].actor_2_facebook_likes,A[len].imdb_score,A[len].aspect_ratio,A[len].movie_facebook_likes) != 28) 
+                       Record[len].color, Record[len].director_name, &(Record[len].num_critic_for_reviews),Record[len].duration,
+                       Record[len].director_facebook_likes,Record[len].actor_3_facebook_likes,Record[len].actor_2_name,
+                       Record[len].actor_1_facebook_likes,Record[len].gross,Record[len].genres,Record[len].actor_1_name,
+                       Record[len].movie_title,Record[len].num_voted_users,Record[len].cast_total_facebook_likes,Record[len].actor_3_name,
+                       Record[len].facenumber_in_poster,Record[len].plot_keywords,Record[len].movie_imdb_link,Record[len].num_user_for_reviews,
+                       Record[len].language,Record[len].country,Record[len].content_rating,Record[len].budget,Record[len].title_year,
+                       Record[len].actor_2_facebook_likes,Record[len].imdb_score,Record[len].aspect_ratio,Record[len].movie_facebook_likes) != 28) 
                         {
+                            //if there is empty data in line, put the it in "incomplete" member variable from struct
                             if (sscanf(line, "%190[^,\n],%190[^,\n],%190[^,\n],%190[^,\n],%230[^,\n],%490[^,\n],%490[^,\n],%490[^,\n],%490[^,\n],%490[^,\n],%490[^,\n],\"%490[^\"]\",%490[^,\n],%490[^,\n],%490[^,\n],%490[^,\n],%400[^,\n],%499[^,\n],%490[^,\n],%490[^,\n],%490[^,\n],%490[^,\n],%400[^,\n],%490[^,\n],%490[^,\n],%490[^,\n],%490[^,\n],%490[^,\n]",
-                       A[len].color, A[len].director_name, &(A[len].num_critic_for_reviews),A[len].duration,
-                       A[len].director_facebook_likes,A[len].actor_3_facebook_likes,A[len].actor_2_name,
-                       A[len].actor_1_facebook_likes,A[len].gross,A[len].genres,A[len].actor_1_name,
-                       A[len].movie_title,A[len].num_voted_users,A[len].cast_total_facebook_likes,A[len].actor_3_name,
-                       A[len].facenumber_in_poster,A[len].plot_keywords,A[len].movie_imdb_link,A[len].num_user_for_reviews,
-                       A[len].language,A[len].country,A[len].content_rating,A[len].budget,A[len].title_year,
-                       A[len].actor_2_facebook_likes,A[len].imdb_score,A[len].aspect_ratio,A[len].movie_facebook_likes) != 28) 
+                       Record[len].color, Record[len].director_name, &(Record[len].num_critic_for_reviews),Record[len].duration,
+                       Record[len].director_facebook_likes,Record[len].actor_3_facebook_likes,Record[len].actor_2_name,
+                       Record[len].actor_1_facebook_likes,Record[len].gross,Record[len].genres,Record[len].actor_1_name,
+                       Record[len].movie_title,Record[len].num_voted_users,Record[len].cast_total_facebook_likes,Record[len].actor_3_name,
+                       Record[len].facenumber_in_poster,Record[len].plot_keywords,Record[len].movie_imdb_link,Record[len].num_user_for_reviews,
+                       Record[len].language,Record[len].country,Record[len].content_rating,Record[len].budget,Record[len].title_year,
+                       Record[len].actor_2_facebook_likes,Record[len].imdb_score,Record[len].aspect_ratio,Record[len].movie_facebook_likes) != 28) 
                         {
-                            strcpy(A[count++].incomplete ,line);
-                            // printf("%s\n\n", A[count].incomplete );
-                            olololo--;
+                            strcpy(Record[count++].incomplete ,line);
+                            true_size--;
 
                         }
                         else
@@ -65,27 +93,34 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    if (strcmp(argv[1],"-c")==0 ) 
+    if (strcmp(argv[1],"-c")== 0 ) 
     {
-
+        //temporary struct item_t pointer for sorting purpose in Mergesort.c
         struct item_t *tmp =  (struct item_t*) malloc(5000 * sizeof(struct item_t*));
-        char *what;
-        what = argv[2];
+        
+        //which column to sort
+        char *column;
+        column = argv[2];
  
-    printf("Array before sorting:\n");
-    display(A,olololo,token,count);
+    printf("Recordrray before sorting:\n");
+    display(Record,true_size,token,count);
 
  
-    merge_sort(A, tmp, olololo, what);
+    //calls merge functions from Mergesort.c
+    merge_sort(Record, tmp, true_size, column);
  
-    printf("Array after sorting:\n");
-    display(A,olololo,token,count);
+    printf("Recordrray after sorting:\n");
+    display(Record,true_size,token,count);
     free(tmp);
 
 
         
+    }
+    else
+    {
+        printf("%s\n", "Error! : \"-c\" must be typed before indicating which column to sort." );
     } 
-    free(A);
+    free(Record);
 
     
 
